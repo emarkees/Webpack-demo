40f55138d9df6b8913dfd3f022f0dda23a22b0c1
emarkees <emarkees.com>
2023-04-05 03:41:46 -0600

    Import style.css to index.js files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 import printMe from './print.js';
 
 function component() {
@@ -7,6 +8,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
