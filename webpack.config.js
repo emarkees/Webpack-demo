71fd01cbccb4db1bcf662489a227be16300d7bf2
emarkees <emarkees.com>
2023-04-05 03:39:15 -0600

    Add style modile to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   optimization: {
     runtimeChunk: 'single',
   },
